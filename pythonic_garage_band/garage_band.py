de7136b323799b1187437781feb1dd43762be6bb
Haley Griffin <haleygriffin708@gmail.comgit config --global user.name h-griffin>
2020-05-14 14:09:31 -0700

    before lecture

---
+++ pythonic_garage_band/garage_band.py
@@ -0,0 +1,30 @@
+class Band:
+  def __init__(self, name):
+    self.name = 'this is a band name'
+    self.members = members
+    self.play_solo = play_solo
+
+class Musician():
+  self.name = name
+  members = []
+
+class Guitarist(self, name, instrument):
+  self.name = name
+  self.instrument = instrument
+
+  def get_instrument():
+    print('guitar')
+    # return f'my instrument is {self.instrument}'
+
+  def play_solo():
+    print('shredding guitar solo')
+
+class Bassist(self, name, instrument):
+  pass
+
+class Drummer(self, name, instrument):
+  pass
+
+if __name__ == "__main__":
+    yardbirds = Band('Yardbirds')
+    print(yardbirds.greet())
