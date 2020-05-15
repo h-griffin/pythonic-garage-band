e7d336ba74f828e8301d6c4aedc2486861cc32da
Haley Griffin <haleygriffin708@gmail.comgit config --global user.name h-griffin>
2020-05-14 17:47:40 -0700

    tests incomplete

--- pythonic_garage_band/garage_band.py
+++ pythonic_garage_band/garage_band.py
@@ -1,30 +1,76 @@
 class Band:
-  def __init__(self, name):
-    self.name = 'this is a band name'
+  band_list = []
+
+  def __init__(self, name='none', members='[jane, john]'):
+    self.name = name    #'this is a band name'   #band name / slipknot
     self.members = members
-    self.play_solo = play_solo
+    Band.band_list.append(self)
 
+  # def play_solo(self):
+  #   pass
+
+  def __str__(self):
+    return f'str band name: {self.name}'
+
+  def __repr__(self):
+    return f'repr band name: {self.name}'
+
+  @classmethod
+  def to_list(cls):
+    print(cls)
+    return cls.band_list    #return all created bands Band.to_list()
+
+  @staticmethod
+  def will_never_change(self):    #is not modified by state of class ^^
+    pass
+    
 class Musician():
-  self.name = name
-  members = []
+  musician_list = []
+  #super().__init__(name) //    things need to happen in the super class init
+  def __init__(self, name='none'):
+    self.name = name
+    Musician.musician_list.append(self)
 
-class Guitarist(self, name, instrument):
-  self.name = name
-  self.instrument = instrument
+  def __str__(self):
+    return f'str Musician name: {self.name}'
 
-  def get_instrument():
-    print('guitar')
-    # return f'my instrument is {self.instrument}'
+  def __repr__(self):
+    return f'repr Musician name: {self.name}'
 
-  def play_solo():
-    print('shredding guitar solo')
 
-class Bassist(self, name, instrument):
-  pass
+  @classmethod
+  def to_list(cls):
+    return cls.musician_list
+    
 
-class Drummer(self, name, instrument):
-  pass
+class Guitarist():
+  def __init__(self, name='none'):
+    self.name = name    #member name / cory taylor
+  def get_instrument(self):
+    return 'guitar'
+  def play_solo(self):
+    return 'shredding guitar solo'
+
+class Bassist():
+  def __init__(self, name='none'):
+    self.name = name
+  def get_instrument(self):
+    return 'bass'
+  def play_solo(self):
+    return 'thundering bass solo'
+
+class Drummer():
+  def __init__(self, name='none'):
+    self.name = name
+  def get_instrument(self):
+    return 'drums'
+  def play_solo(self):
+    return 'booming drum solo'
 
 if __name__ == "__main__":
-    yardbirds = Band('Yardbirds')
-    print(yardbirds.greet())
+  slipknot = Band('Slipknot', ['corey, james'])
+  corey = Musician('Corey')
+  print(slipknot.__str__())
+  print(slipknot.__repr__())
+  print(corey.__str__())
+  print(corey.__repr__())
--- tests/test_pythonic_garage_band.py
+++ tests/test_pythonic_garage_band.py
@@ -1,6 +1,37 @@
+# import pytest
 from pythonic_garage_band import __version__
-
+from pythonic_garage_band.garage_band import Band, Musician, Guitarist, Bassist, Drummer
+# from pythonic_garage_band.garage_band import Musician
+# from pythonic_garage_band.garage_band import Guitarist
+# from pythonic_garage_band.garage_band import Bassist
+# from pythonic_garage_band.garage_band import Drummer
 
 def test_version():
     assert __version__ == '0.1.0'
 
+def test_band_list():
+  assert Band.to_list() == []
+  slipknot = Band('Slipknot', '[Corey, James]')
+  assert len(Band.to_list()) == 1
+  
+def test_band_list_attributes():
+  Band.band_list = []
+  Musician('Corey')
+  # assert Musician.musician_list()[0].name == 'Corey'
+  assert len(Musician.to_list()) == 1
+
+def test_play_solo():
+  
+# @pytest.fixture(autouse=True)
+# def prep():
+#   Band.count = 0
+#   Band.to_list = []
+  # def test_(prep) / autouse
+
+# @pytest.fixture
+# def band():
+#   return Band('Nirvana', [
+#     Guitarist('Kurt Cobain'),
+#     Bassist('Krist Novoselic'),
+#     Drummer('Dave Grohl')
+#   ])
